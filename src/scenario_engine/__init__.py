6bc651af86cbdb7cc804cfd7153a28e882fc4b76
pratikoperations <pratikpatilcv@gmail.com>
2026-07-12 11:51:19 +0530

    PVE-0.5: add scenario recommendation and UI

---
+++ src/scenario_engine/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic scenario-analysis utilities for PVE."""
+
+from .engine import AlternativeScenario, ScenarioInputs, ScenarioResult, evaluate_scenario
+
+__all__ = ["AlternativeScenario", "ScenarioInputs", "ScenarioResult", "evaluate_scenario"]
