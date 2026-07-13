4bf93a6ad7d8e6ba153857383a3b44e5386198e2
pratikoperations <pratikpatilcv@gmail.com>
2026-07-13 14:04:07 +0530

    PVE 1.1 Build 3: export category requirement models

--- src/category_registry/__init__.py
+++ src/category_registry/__init__.py
@@ -1,4 +1,11 @@
-from .models import CategoryDefinition
+from .models import CategoryDefinition, DocumentDefinition, FieldDefinition, TestDefinition
 from .registry import CategoryRegistry, default_registry
 
-__all__ = ["CategoryDefinition", "CategoryRegistry", "default_registry"]
+__all__ = [
+    "CategoryDefinition",
+    "DocumentDefinition",
+    "FieldDefinition",
+    "TestDefinition",
+    "CategoryRegistry",
+    "default_registry",
+]
