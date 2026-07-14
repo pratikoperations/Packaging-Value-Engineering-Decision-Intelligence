dcb1ca24ffab5b0b5cae6947139852331d00b643
pratikoperations <pratikpatilcv@gmail.com>
2026-07-14 10:48:22 +0530

    Export corrugated evidence and capability APIs

--- src/category_registry/__init__.py
+++ src/category_registry/__init__.py
@@ -1,3 +1,18 @@
+from .corrugated_evidence import (
+    CAPABILITY_OUTCOMES,
+    EVIDENCE_CONFIDENCE,
+    SOURCE_CLASSIFICATIONS,
+    SUPPLIER_CAPABILITY_FIELDS,
+    TECHNICAL_REQUIREMENT_FIELDS,
+    CapabilityAssessment,
+    EvidenceConfidenceAssessment,
+    EvidenceMatch,
+    assess_evidence_confidence,
+    assess_supplier_capability,
+    detect_conflicting_evidence,
+    match_evidence,
+    technical_requirement_profile,
+)
 from .corrugated_specification import (
     BOX_STYLES,
     CLOSURE_METHODS,
@@ -14,20 +29,13 @@ from .models import CategoryDefinition, DocumentDefinition, FieldDefinition, Tes
 from .registry import CategoryRegistry, default_registry
 
 __all__ = [
-    "BOX_STYLES",
-    "CLOSURE_METHODS",
-    "CONVERTING_PROFILES",
-    "JOINT_TYPES",
-    "PRINT_PROCESSES",
-    "SpecificationDifference",
-    "SpecificationTolerance",
-    "compare_specifications",
-    "validate_tolerance",
-    "validate_tolerances",
-    "CategoryDefinition",
-    "DocumentDefinition",
-    "FieldDefinition",
-    "TestDefinition",
-    "CategoryRegistry",
-    "default_registry",
+    "CAPABILITY_OUTCOMES", "EVIDENCE_CONFIDENCE", "SOURCE_CLASSIFICATIONS",
+    "SUPPLIER_CAPABILITY_FIELDS", "TECHNICAL_REQUIREMENT_FIELDS",
+    "CapabilityAssessment", "EvidenceConfidenceAssessment", "EvidenceMatch",
+    "assess_evidence_confidence", "assess_supplier_capability",
+    "detect_conflicting_evidence", "match_evidence", "technical_requirement_profile",
+    "BOX_STYLES", "CLOSURE_METHODS", "CONVERTING_PROFILES", "JOINT_TYPES", "PRINT_PROCESSES",
+    "SpecificationDifference", "SpecificationTolerance", "compare_specifications",
+    "validate_tolerance", "validate_tolerances", "CategoryDefinition", "DocumentDefinition",
+    "FieldDefinition", "TestDefinition", "CategoryRegistry", "default_registry",
 ]
