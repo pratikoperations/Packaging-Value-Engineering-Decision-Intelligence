a9d1407e82d1d1a303fcc52aa4314c448d349fd0
pratikoperations <pratikpatilcv@gmail.com>
2026-07-14 16:06:56 +0530

    Add Build 5 defect taxonomy package

---
+++ src/defect_taxonomy/__init__.py
@@ -0,0 +1,8 @@
+from .models import TaxonomyValidation, TaxonomyIssue, validate_complaint_record, validate_defect_classification
+
+__all__ = [
+    "TaxonomyValidation",
+    "TaxonomyIssue",
+    "validate_complaint_record",
+    "validate_defect_classification",
+]
