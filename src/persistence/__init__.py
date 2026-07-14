a0e7bdde25411922dc83ac0f69fc82e2b8ca393e
pratikoperations <pratikpatilcv@gmail.com>
2026-07-14 16:13:08 +0530

    Export Build 5 taxonomy repositories

--- src/persistence/__init__.py
+++ src/persistence/__init__.py
@@ -1,6 +1,7 @@
 from .database import Database
 from .dataset_repository import DatasetRepository
 from .decision_repository import DecisionRepository
+from .defect_taxonomy_repository import ComplaintRecordRepository, DefectClassificationRepository
 from .drawing_evidence_repository import DrawingEvidenceRepository
 from .export_repository import ExportRepository
 from .project_repository import ProjectRepository
@@ -12,9 +13,11 @@ from .trial_execution_repository import TrialExecutionRepository
 from .trial_plan_repository import TrialPlanRepository
 
 __all__ = [
+    "ComplaintRecordRepository",
     "Database",
     "DatasetRepository",
     "DecisionRepository",
+    "DefectClassificationRepository",
     "DrawingEvidenceRepository",
     "ExportRepository",
     "ProjectRepository",
