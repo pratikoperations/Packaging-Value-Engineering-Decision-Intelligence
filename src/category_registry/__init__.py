ff2c7881759331240ff112ffbfbbcd11113b22bf
pratikoperations <pratikpatilcv@gmail.com>
2026-07-14 10:38:25 +0530

    Export corrugated specification configuration

--- src/category_registry/__init__.py
+++ src/category_registry/__init__.py
@@ -1,7 +1,29 @@
+from .corrugated_specification import (
+    BOX_STYLES,
+    CLOSURE_METHODS,
+    CONVERTING_PROFILES,
+    JOINT_TYPES,
+    PRINT_PROCESSES,
+    SpecificationDifference,
+    SpecificationTolerance,
+    compare_specifications,
+    validate_tolerance,
+    validate_tolerances,
+)
 from .models import CategoryDefinition, DocumentDefinition, FieldDefinition, TestDefinition
 from .registry import CategoryRegistry, default_registry
 
 __all__ = [
+    "BOX_STYLES",
+    "CLOSURE_METHODS",
+    "CONVERTING_PROFILES",
+    "JOINT_TYPES",
+    "PRINT_PROCESSES",
+    "SpecificationDifference",
+    "SpecificationTolerance",
+    "compare_specifications",
+    "validate_tolerance",
+    "validate_tolerances",
     "CategoryDefinition",
     "DocumentDefinition",
     "FieldDefinition",
