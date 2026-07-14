66cc09f1ba8a84946d97de2ca0d51be9b2feb3af
pratikoperations <pratikpatilcv@gmail.com>
2026-07-14 15:28:48 +0530

    PVE 1.3 Build 3: add trial planning package

---
+++ src/trial_planning/__init__.py
@@ -0,0 +1,5 @@
+"""Governed trial planning and validation requirements."""
+
+from .models import TrialPlanIssue, TrialPlanValidation, validate_trial_plan
+
+__all__ = ["TrialPlanIssue", "TrialPlanValidation", "validate_trial_plan"]
