d465719c62fe932beff2df50423b8faca26b7bf9
pratikoperations <pratikpatilcv@gmail.com>
2026-07-27 19:58:43 +0530

    Build U6: export unified specification snapshot boundary

--- src/specification_intake/__init__.py
+++ src/specification_intake/__init__.py
@@ -11,15 +11,30 @@ from .review_view import (
     load_field_registry,
     normalize_corrected_value,
 )
+from .snapshot import (
+    ALIAS_REGISTRY_VERSION,
+    EXTRACTION_SCHEMA_VERSION,
+    PROVIDER_ID,
+    SnapshotDocument,
+    SnapshotField,
+    UnifiedSpecificationSnapshot,
+    build_unified_snapshot,
+)
 
 __all__ = [
+    "ALIAS_REGISTRY_VERSION",
     "CommonReviewView",
     "DocumentRole",
+    "EXTRACTION_SCHEMA_VERSION",
+    "PROVIDER_ID",
     "PairFormat",
+    "SnapshotDocument",
+    "SnapshotField",
     "SpecificationPair",
     "UnifiedCanonicalDraft",
     "UnifiedSourceBlock",
     "UnifiedSpecificationDocument",
+    "UnifiedSpecificationSnapshot",
     "adapt_docx",
     "adapt_pdf",
     "adapt_specification",
@@ -28,6 +43,7 @@ __all__ = [
     "build_common_review_views",
     "build_pair",
     "build_unified_canonical_draft",
+    "build_unified_snapshot",
     "classify_pair",
     "deterministic_candidates",
     "load_field_registry",
