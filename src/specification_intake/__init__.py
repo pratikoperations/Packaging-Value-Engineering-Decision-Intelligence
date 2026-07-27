20b1fd5abfecdcff1cafe4b148ea23a46f251dbb
pratikoperations <pratikpatilcv@gmail.com>
2026-07-27 18:03:02 +0530

    Build U4: export common review boundary

--- src/specification_intake/__init__.py
+++ src/specification_intake/__init__.py
@@ -1,8 +1,17 @@
 from .adapters import adapt_docx, adapt_pdf, adapt_specification
 from .models import DocumentRole, PairFormat, UnifiedSourceBlock, UnifiedSpecificationDocument
 from .pairing import SpecificationPair, build_pair, classify_pair
+from .review_view import (
+    CommonReviewView,
+    all_reviews_resolved,
+    apply_review_action,
+    build_common_review_views,
+    deterministic_candidates,
+    load_field_registry,
+)
 
 __all__ = [
+    "CommonReviewView",
     "DocumentRole",
     "PairFormat",
     "SpecificationPair",
@@ -11,6 +20,11 @@ __all__ = [
     "adapt_docx",
     "adapt_pdf",
     "adapt_specification",
+    "all_reviews_resolved",
+    "apply_review_action",
+    "build_common_review_views",
     "build_pair",
     "classify_pair",
+    "deterministic_candidates",
+    "load_field_registry",
 ]
