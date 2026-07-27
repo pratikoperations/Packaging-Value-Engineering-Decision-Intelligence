8c3876abbf40c3baf5913b12d199dc472a85d3cb
pratikoperations <pratikpatilcv@gmail.com>
2026-07-27 14:17:06 +0530

    feat(pve2.1): expose PDF snapshot boundary

--- src/pdf_intake/__init__.py
+++ src/pdf_intake/__init__.py
@@ -17,9 +17,17 @@ from .models import (
     ValidatedPdf,
 )
 from .pdf_parser import PDF_PARSER_VERSION, normalize_pdf_text, parse_validated_pdf
+from .snapshot import (
+    ConfirmedPdfField,
+    ConfirmedPdfSnapshot,
+    build_confirmed_pdf_snapshot,
+    build_pdf_canonical_dataset_draft,
+)
 from .source_blocks import make_pdf_source_block_id
 
 __all__ = [
+    "ConfirmedPdfField",
+    "ConfirmedPdfSnapshot",
     "PDF_PARSER_VERSION",
     "ParsedPdf",
     "PdfEligibility",
@@ -29,6 +37,8 @@ __all__ = [
     "PdfSourceBlock",
     "PdfValidationError",
     "ValidatedPdf",
+    "build_confirmed_pdf_snapshot",
+    "build_pdf_canonical_dataset_draft",
     "build_pdf_review_bundle",
     "compare_pdf_review_groups",
     "extract_pdf_document",
