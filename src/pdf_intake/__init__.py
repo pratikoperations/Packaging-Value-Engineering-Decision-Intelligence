a1a828d171354c0f668ee7a355ca351cb57da394
pratikoperations <pratikpatilcv@gmail.com>
2026-07-27 13:50:09 +0530

    feat(pve2.1): expose page-aware PDF parser API

--- src/pdf_intake/__init__.py
+++ src/pdf_intake/__init__.py
@@ -1,12 +1,27 @@
 """PVE 2.1 searchable digital PDF intake boundary."""
 
 from .file_validation import PdfValidationError, validate_pdf, validate_pdf_pair
-from .models import PdfEligibility, ValidatedPdf
+from .models import (
+    ParsedPdf,
+    PdfEligibility,
+    PdfLayoutWarning,
+    PdfSourceBlock,
+    ValidatedPdf,
+)
+from .pdf_parser import PDF_PARSER_VERSION, normalize_pdf_text, parse_validated_pdf
+from .source_blocks import make_pdf_source_block_id
 
 __all__ = [
+    "PDF_PARSER_VERSION",
+    "ParsedPdf",
     "PdfEligibility",
+    "PdfLayoutWarning",
+    "PdfSourceBlock",
     "PdfValidationError",
     "ValidatedPdf",
+    "make_pdf_source_block_id",
+    "normalize_pdf_text",
+    "parse_validated_pdf",
     "validate_pdf",
     "validate_pdf_pair",
 ]
