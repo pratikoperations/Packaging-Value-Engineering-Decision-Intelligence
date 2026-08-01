d6b996d1f893f1729ce5118e13bf4a765c56367a
pratikoperations <pratikpatilcv@gmail.com>
2026-08-01 16:00:36 +0530

    Add governed browser acceptance package

---
+++ src/browser_acceptance/__init__.py
@@ -0,0 +1,6 @@
+"""Governed local browser-acceptance utilities."""
+
+from .contracts import PAGE_CONTRACTS, SIDEBAR_GROUPS, VIEWPORTS
+from .process_manager import StreamlitProcess
+
+__all__ = ["PAGE_CONTRACTS", "SIDEBAR_GROUPS", "VIEWPORTS", "StreamlitProcess"]
